fe65d7212170f192882390b021c58b96862b67a2
Michael <mcdelaurier@gmail.com>
2018-12-03 19:27:44 -0800

    New Course

--- Game Project/Combat v1/sketch.js
+++ Game Project/Combat v1/sketch.js
@@ -1,89 +1,104 @@
 let t;
 let bullet = [];
-
+var canH = 586
+var canW = 900
 function setup() {
-  createCanvas(windowWidth, windowHeight);
 
-  t = new Tank(createVector(windowWidth/2, windowHeight/2), 0, 0.05, createVector(0,0), 0);
+  createCanvas(canW,canH);
+
+  t1 = new Tank(255,0,0,createVector(65, 295), 0, 0.05, createVector(0,0), 0);
+  t2 = new Tank(0,0,255,createVector(835, 295), 0, 0.05, createVector(0,0), 0);
 }
 
 function draw() {
-  background(255);
-
-  t.drawTank();
-
-  fill(254,0,0);
-  rect(400,100,20,200);
-
-  t.detectWall();
-  t.moveMe();
-
-
-  t.updatePos();
-
-  noStroke();
+  background(187, 207, 82);
 
   for (let i = 0; i < bullet.length; i++) {
       bullet[i].drawBullet();
       bullet[i].moveBullet();
     }
 
-    console.log(t.heading)
+  t1.drawTank();
+  t2.drawTank();
+  noStroke()
+  drawCourse()
+  t1.detectWall();
+  t2.detectWall();
+  t1.moveMe2();
+  t2.moveMe1();
+
+
+  t1.updatePos();
+  t2.updatePos();
+
+  noStroke();
+  fill(0,0,0);
+  textSize(32);
+  text('0', canW/4, 40);
+  text('0', (canW/4)*3, 40);
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
   if (keyCode === 191) {
-    let  b = new Bullet(mouseX, mouseY,0,0,0,random(-5,5),random(-5,5),8);
+    let  b = new Bullet(t1.pos.x,t1.pos.y,0,0,0,p5.Vector.fromAngle(t1.heading),8);
     bullet.push(b);
     //console.log(bullet);
   }
 }
 
 class Bullet {
-  constructor(x,y,r,g,b,speedx,speedy,w){ //every ball needs an x value and a y value
-        this.x = x;
-        this.y = y;
+  constructor(bPosx, bPosy, r,g,b,bHeading,w){ //every ball needs an x value and a y value
+        this.bPosx = bPosx;
+        this.bPosy = bPosy;
         this.r = r;
         this.g = g;
         this.b = b;
-        this.speedx = speedx;
-        this.speedy = speedy;
+        this.bHeading = bHeading
         this.w = w
+
   }
 
   drawBullet() {  // draw a ball on the screen at x,y
+        push();
+        translate(this.bPosx,this.bPosy);
         strokeWeight(1);
+        stroke(0,0,0)
         fill(this.r,this.g,this.b);
-        ellipse(this.x ,this.y,this.w,this.w);
+        ellipse(0 , 0,this.w,this.w);
+        pop();
   }
 
   moveBullet(){ //update the location of the ball, so it moves across the screen
-
-    this.x = this.x+this.speedx;
-    this.y = this.y+this.speedy;
+    this.bHeading  = this.bHeading.mult(1.05)
+    this.bPosx = this.bPosx+this.bHeading.x;
+    this.bPosy = this.bPosy+this.bHeading.y;
   }
 
 }
 
 class Tank {
-  constructor(pos, heading, angle, vel, force) {
+  constructor(r, g, b, pos, heading, angle, vel, force) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
     this.pos = pos;
     this.heading = heading;
     this.angle = angle;
     this.vel = vel;
     this.force = force;
-    this.testTopRight
-    this.testBotRight
-    this.testTopLeft
-    this.testBotLeft
+
+    this.testTopRight;
+    this.testBotRight;
+    this.testTopLeft;
+    this.testBotLeft;
 
   }
 
   // Draw the tank at 0,0, then translate to the current position, also rotate
   drawTank() {
-    stroke("black");
+    stroke(this.r,this.g,this.b);
     strokeWeight(1);
-    fill(0,0,0);
+    fill(this.r,this.g,this.b);
     push();
     translate(this.pos.x,this.pos.y);
     rotate(this.heading - PI/2)
@@ -106,12 +121,12 @@ class Tank {
     //console.log(this.testTopRight);
   }
 
-  moveMe(){
+  moveMe1(){
 
     if (keyIsDown(UP_ARROW) && (this.testTopRight[0] != 254) && (this.testBotRight[0] != 254) && (this.testTopLeft[0] != 254) && (this.testBotLeft[0] != 254))  { //if you hold the up arrow, move up by speed
 
       var force = p5.Vector.fromAngle(this.heading);
-      force.mult(2);
+      force.mult(1.5);
       this.vel = force;
 
     } else if (keyIsDown(UP_ARROW)) {
@@ -134,4 +149,64 @@ class Tank {
 
   }
 
+  moveMe2(){
+
+    if (keyIsDown(87) && (this.testTopRight[0] != 254) && (this.testBotRight[0] != 254) && (this.testTopLeft[0] != 254) && (this.testBotLeft[0] != 254))  { //if you hold the up arrow, move up by speed
+
+      var force = p5.Vector.fromAngle(this.heading);
+      force.mult(1.5);
+      this.vel = force;
+
+    } else if (keyIsDown(87)) {
+      var direction = p5.Vector.fromAngle(this.heading + PI);
+      this.pos.x += direction.x*20;
+      this.pos.y += direction.y*20;
+
+    } else {
+      this.vel = createVector(0,0);
+      //console.log("stopped");
+    }
+
+    if (keyIsDown(65)) { // if you hold the down arrow, move down by speed
+        this.heading -= this.angle
+    }
+
+    if (keyIsDown(68)) { // if you hold the down arrow, move down by speed
+        this.heading += this.angle
+    }
+
+  }
+
+}
+
+function drawCourse() {
+  fill(254, 176, 91);
+
+  rect(0,55,canW,20)
+  rect(0,canH-20,canW,20)
+  rect(0,55,20,canH-55)
+  rect(canW-20,55,20,canH-55)
+
+  rect((canW/2)-20,55, 40, 60)
+  rect((canW/2)-20,canH-60, 40, 60)
+
+  rect((canW/4)-20,(canH/2)-20,40,40)
+  rect((canW/4*3)-20,(canH/2)-20,40,40)
+
+  rect((canW/2)-150,canH/4,60,20)
+  rect((canW/2)-150,canH/4,20,60)
+  rect((canW/2)+90,canH/4,60,20)
+  rect((canW/2)+130,canH/4,20,60)
+
+  rect((canW/2)-150,(canH/4*3)+20,60,20)
+  rect((canW/2)-150,(canH/4*3)+20,20,-40)
+  rect((canW/2)+90,(canH/4*3)+20,60,20)
+  rect((canW/2)+130,(canH/4*3)+20,20,-40)
+
+  rect(canW/8, (canH-20)/3, 20,210)
+  rect(canW/8, (canH-20)/3, -20,20)
+  rect(canW/8, (canH-20)/3*2+1, -20,20)
+  rect((canW/8)*7-20, (canH-20)/3, 20,210)
+  rect((canW/8)*7-20, (canH-20)/3, 40,20)
+  rect((canW/8)*7-20, (canH-20)/3*2+1, 40,20)
 }
